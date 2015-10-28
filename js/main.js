7a02bd7aaa2ab49810c76065ac82a301d09216ae
kcin1993 <kcin1993@gmail.com>
2015-10-28 10:34:09 +0800

    add key

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 //google map api
+//Basic informatio of sanxia mp
+//1. (24.942946, 121.368398),
+//2. zoom:14;
+
 function initialize() {
-  var mapProp = {
-    center:new google.maps.LatLng(24.942946, 121.368398),
-    zoom:14,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	var mapCanvas = document.getElementById('googleMap');
+	var mapOptions = {
+		center: new google.maps.LatLng(24.942946, 121.368398),
+		zoom: 15,
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+	}
+	var map = new google.maps.Map(mapCanvas, mapOptions);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
