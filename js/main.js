5cba8321dc66359ee4a0e38b06da32ef5867c6b6
kcin1993 <kcin1993@gmail.com>
2015-10-28 11:14:36 +0800

    add marker

--- js/main.js
+++ js/main.js
@@ -3,7 +3,15 @@
 //1. (24.942946, 121.368398),
 //2. zoom:14;
 
+
+
+var locations = [
+  ["闇黑工廠",     24.944030, 121.374654],
+  ["艾波索",    24.943920, 121.373774]
+];
+
 function initialize() {
+	var infowindow = new google.maps.InfoWindow();
 	var mapCanvas = document.getElementById('googleMap');
 	var mapOptions = {
 		center: new google.maps.LatLng(24.942946, 121.368398),
@@ -11,7 +19,25 @@ function initialize() {
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
 	}
 	var map = new google.maps.Map(mapCanvas, mapOptions);
-}
+	  
+	function placeMarker( loc ) {
+	  var latLng = new google.maps.LatLng( loc[1], loc[2]);
+	  var marker = new google.maps.Marker({
+	    position : latLng,
+	    map      : map
+	  });
+	  google.maps.event.addListener(marker, 'click', function(){
+	      infowindow.close(); // Close previously opened infowindow
+	      infowindow.setContent( "<div id='infowindow'>"+ loc[0] +"</div>");
+	      infowindow.open(map, marker);
+	  });
+	}
+
+	for(var i=0; i<locations.length; i++) {
+	  placeMarker( locations[i] );
+	} 
+}//initialize end
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
 
