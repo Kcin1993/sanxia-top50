223983ad0fb17655cf4c46bddea39436167e7e10
kcin1993 <kcin1993@gmail.com>
2015-11-02 22:16:39 +0800

    add info

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // Google map
 jQuery(function($) {
-    // Asynchronously Load the map API 
     var script = document.createElement('script');
     script.src = "http://maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
     document.body.appendChild(script);
@@ -13,36 +12,35 @@ function initialize() {
         mapTypeId: 'roadmap'
     };
                     
-    // Display a map on the page
     map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
     map.setTilt(45);
         
-    // Multiple Markers
     var markers = [
-        ["闇黑工廠", 24.944030, 121.374654],
-        ["艾波索",24.943920, 121.373774],
+        ["福美軒金牛角", 24.935443, 121.371027],
+        ["榕樹下鮮肉包",24.932174, 121.374947],
     ];
                         
-    // Info Window Content
     var infoWindowContent = [
         [
         '<div class="info_content">' +
-        '<h3>闇黑工廠</h3>' +
-        '<p>text</p>' + 
+        '<h3>福美軒金牛角</h3>' +
+        '<p>特色：三峽金牛角創始店</p>' + 
+        '<p>探索：新北市三峽區信義街25號</p>' + 
+        '<a href="http://www.shockpaper.com/2013/09/%E4%BB%A5%E5%A0%85%E6%8C%81%E6%9B%B8%E5%AF%AB%E4%B8%89%E8%A7%92%E6%B9%A7%E4%BB%A3%E5%90%8D%E8%A9%9E/#more-2242">推薦人：Shock! 三峽客</a>'
         '</div>'
         ],
         [
         '<div class="info_content">' +
-        '<h3>艾波索</h3>' +
+        '<h3>榕樹下鮮肉包</h3>' +
         '<p>text</p>' +
+        '<p>探索：台北縣三峽鎮中華路3巷11之3號</p>' +
+        '<a href="http://www.shockpaper.com/2012/11/%e3%80%90%e5%ba%97%e9%8b%aa%e5%b0%88%e6%ac%84%e3%80%91%e6%a6%95%e6%a8%b9%e4%b8%8b%e9%ae%ae%e8%82%89%e5%8c%85/">推薦人：Shock! 三峽客</a>'
         '</div>'
         ]
     ];
         
-    // Display multiple markers on a map
     var infoWindow = new google.maps.InfoWindow(), marker, i;
     
-    // Loop through our array of markers & place each one on the map  
     for( i = 0; i < markers.length; i++ ) {
         var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
         bounds.extend(position);
@@ -52,7 +50,6 @@ function initialize() {
             title: markers[i][0]
         });
         
-        // Allow each marker to have an info window    
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
                 infoWindow.setContent(infoWindowContent[i][0]);
@@ -60,11 +57,9 @@ function initialize() {
             }
         })(marker, i));
 
-        // Automatically center the map fitting all markers on the screen
         map.fitBounds(bounds);
     }
 
-    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
     var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
         this.setZoom(14);
         google.maps.event.removeListener(boundsListener);
@@ -74,15 +69,24 @@ function initialize() {
 
 
 
-//scroll down
+//scroll 
 $("document").ready(function() {      
-        
-    $('.scrollmap, .scrolldown').click(function(){
+    $('.scrolldown').click(function(){
       $('html, body').animate({
         scrollTop: $("#googleMap").offset().top
       }, 1500);    
      });
+    $('.scrolltop').click(function(){
+      $('html, body').animate({
+        scrollTop: $("#header_wrapper").offset().top
+      }, 1500);    
+     });
 });
+
+
+
+
+
 
 
 //google map api
